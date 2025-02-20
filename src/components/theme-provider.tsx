ee30576e328f8cd827b9fd8e16c8992dc350d732
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-20 18:25:16 +0100

    docs: add description for theme-provider component

--- src/components/theme-provider.tsx
+++ src/components/theme-provider.tsx
@@ -3,6 +3,11 @@
 import { ThemeProvider as NextThemesProvider } from 'next-themes';
 import * as React from 'react';
 
+/**
+ * The theme provider component handles the theme switching logic. This component
+ * expects a `children` prop, which is the content that will be wrapped by the
+ * theme provider.
+ */
 export function ThemeProvider({
   children,
   ...props
