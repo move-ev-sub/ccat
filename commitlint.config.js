aeff0a97482878678b10be6b524a4d5203b9ba60
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-20 16:25:32 +0100

    docs: extend commitlint explainations

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,12 +1,16 @@
 // build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
+// chore: Other changes that don't modify src or test files
 // ci: Changes to our CI configuration files and scripts (example scopes: Travis, Circle, BrowserStack, SauceLabs)
 // docs: Documentation only changes
 // feat: A new feature
 // fix: A bug fix
 // perf: A code change that improves performance
 // refactor: A code change that neither fixes a bug nor adds a feature
+// revert: Reverts a previous commit
 // style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
 // test: Adding missing tests or correcting existing tests
+// translation: Translation updates
+// security: Security updates
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
