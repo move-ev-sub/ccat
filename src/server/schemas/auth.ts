cc536bc5eef672356f531873a25d89663c34007c
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-23 20:43:45 +0100

    fix: update form component styles to use correct class names

--- src/server/schemas/auth.ts
+++ src/server/schemas/auth.ts
@@ -9,7 +9,9 @@ export type SignUpData = z.infer<typeof signUpSchema>;
 
 export const loginSchema = z.object({
   email: z.string().email(),
-  password: z.string().min(6),
+  password: z
+    .string()
+    .min(6, { message: 'Passwort muss mindestens 6 Zeichen lang sein.' }),
 });
 
 export type LoginData = z.infer<typeof loginSchema>;
