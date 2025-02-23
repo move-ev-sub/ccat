6b88f72874ee019634e6e144ad6d0bb4c890e65c
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-23 20:38:07 +0100

    feat: add register form

---
+++ src/components/forms/register.tsx
@@ -0,0 +1,78 @@
+'use client';
+
+import { signUpSchema } from '@/server/schemas/auth';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { signup } from '@/server/actions/auth';
+import React from 'react';
+
+export function RegisterForm() {
+  const [loading, setLoading] = React.useState(false);
+
+  const form = useForm<z.infer<typeof signUpSchema>>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof signUpSchema>) {
+    setLoading(true);
+
+    const res = await signup(values);
+    if (res) {
+      console.log(res);
+    }
+
+    setLoading(false);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="max@mustermann.de" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Passwort</FormLabel>
+              <FormControl>
+                <Input type="password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={loading}>
+          Registrieren
+        </Button>
+      </form>
+    </Form>
+  );
+}
