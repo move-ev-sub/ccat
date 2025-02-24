69cab1f05612a6dedc66fda859c74bb3a71608aa
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-24 15:07:17 +0100

    fix: update admin layout to include dropdown menu

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,4 +1,8 @@
 import { Navbar } from '@/components/navbar';
+import {
+  AdminDropdownMenu,
+  UserDropdownMenu,
+} from '@/components/profile-dropdown-menu';
 import { TopNav, TopNavItem } from '@/components/ui/top-nav';
 import React from 'react';
 
@@ -7,12 +11,14 @@ export default async function AdminLayout({
 }: React.PropsWithChildren) {
   return (
     <main>
-      <Navbar>
-        <TopNav>
+      <Navbar className="flex">
+        <TopNav className="mr-auto">
           <TopNavItem href={'#'}>Home</TopNavItem>
           <TopNavItem href={'#'}>About</TopNavItem>
           <TopNavItem href={'#'}>Contact</TopNavItem>
         </TopNav>
+        <UserDropdownMenu />
+        <AdminDropdownMenu />
       </Navbar>
       {children}
     </main>
