4553c70e7e3efad2d11fa09b120e4613aa87f26f
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-25 23:42:16 +0100

    feat: implement base layout for event specific admin routes

--- src/app/admin/event/[eventId]/layout.tsx
+++ src/app/admin/event/[eventId]/layout.tsx
@@ -1,16 +1,24 @@
 import { EventSwitch } from '@/components/event-switch';
 import { Navbar } from '@/components/navbar';
-import { AdminMobileNav, NavigationItem } from '@/components/navigation';
+import {
+  AdminMobileNav,
+  NavigationItem,
+  SubNavigation,
+  SubNavigationItem,
+} from '@/components/navigation';
 import { getAllEvents } from '@/server/actions/event';
 import React from 'react';
 
 export default async function AdminEventLayout({
   children,
+  params,
 }: React.PropsWithChildren & {
   params: Promise<{
     eventId: string;
   }>;
 }) {
+  const eventId = (await params).eventId;
+
   const res = await getAllEvents();
 
   if (res.error) {
@@ -39,6 +47,27 @@ export default async function AdminEventLayout({
           events={res.data || []}
         />
       </Navbar>
+      <SubNavigation>
+        <SubNavigationItem base={`/admin/event/${eventId}`} href={`/`}>
+          Übersicht
+        </SubNavigationItem>
+        <SubNavigationItem
+          base={`/admin/event/${eventId}`}
+          href={'/sub-events'}
+        >
+          Unterveranstaltungen
+        </SubNavigationItem>
+        <SubNavigationItem
+          base={`/admin/event/${eventId}`}
+          href={'/applications'}
+        >
+          Bewerbungen
+        </SubNavigationItem>
+
+        <SubNavigationItem base={`/admin/event/${eventId}`} href={'/settings'}>
+          Einstellungen
+        </SubNavigationItem>
+      </SubNavigation>
       {children}
     </>
   );
--- src/app/admin/event/[eventId]/page.tsx
+++ src/app/admin/event/[eventId]/page.tsx
@@ -1,5 +1,27 @@
-export default async function AdminEventOverviewPage({}: {
+import { getEvent } from '@/server/actions/event';
+import { notFound } from 'next/navigation';
+
+export default async function AdminEventOverviewPage({
+  params,
+}: {
   params: Promise<{
     eventId: string;
   }>;
-}) {}
+}) {
+  const eventId = (await params).eventId;
+
+  const res = await getEvent(eventId);
+
+  if (res.error || !res.data) {
+    notFound();
+  }
+
+  return (
+    <div className="py-20">
+      <div className="container">
+        <h1>{res.data.name}</h1>
+        <p>{res.data.description}</p>
+      </div>
+    </div>
+  );
+}
