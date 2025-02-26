ff5dd1bda728294a63528291eb079c06645b2c67
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-02-26 13:20:52 +0100

    feat: implement event filtering for admin overview page

--- src/app/admin/(general)/page.tsx
+++ src/app/admin/(general)/page.tsx
@@ -3,6 +3,15 @@ import { NewEventCard } from '@/components/new-event-card';
 import { PageContainer } from '@/components/page-container';
 import { PageTitle } from '@/components/page-header';
 import { getAllEvents } from '@/server/actions/event';
+import { cn } from '@/utils';
+import {
+  ArchiveBoxIcon,
+  CalendarIcon,
+  EyeIcon,
+  InboxIcon,
+} from '@heroicons/react/16/solid';
+import * as TabsPrimitive from '@radix-ui/react-tabs';
+import React from 'react';
 
 /**
  * The admin overview page where all events are displayed.
@@ -18,17 +27,109 @@ export default async function AdminOverviewPage() {
   return (
     <PageContainer>
       <PageTitle>Veranstaltungen</PageTitle>
-      <div className="mt-12 grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3">
-        <NewEventCard href={'/admin/new/event'} />
-        {events.data &&
-          events.data.map((event) => (
-            <EventThumbnailCard
-              href={`/admin/event/${event.id}`}
-              key={event.id}
-              event={event}
-            />
-          ))}
-      </div>
+      <TabsPrimitive.Tabs className="mt-12" defaultValue="published">
+        <TabsPrimitive.List className="flex flex-wrap items-center justify-start gap-2.5">
+          <EventsTabsTrigger value="all">
+            <CalendarIcon className="size-4" />
+            Alle
+          </EventsTabsTrigger>
+          <EventsTabsTrigger value="published">
+            <EyeIcon className="size-4" />
+            Veröffentlicht
+          </EventsTabsTrigger>
+          <EventsTabsTrigger value="draft">
+            <InboxIcon className="size-4" />
+            Entwürfe
+          </EventsTabsTrigger>
+          <EventsTabsTrigger value="archived">
+            <ArchiveBoxIcon className="size-4" />
+            Archiviert
+          </EventsTabsTrigger>
+        </TabsPrimitive.List>
+        <div className="mt-8">
+          <TabsPrimitive.Content
+            value="all"
+            className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"
+          >
+            <NewEventCard href={'/admin/new/event'} />
+
+            {events.data &&
+              events.data.map((event) => (
+                <EventThumbnailCard
+                  href={`/admin/event/${event.id}`}
+                  key={event.id}
+                  event={event}
+                />
+              ))}
+          </TabsPrimitive.Content>
+          <TabsPrimitive.Content
+            value="published"
+            className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"
+          >
+            <NewEventCard href={'/admin/new/event'} />
+
+            {events.data &&
+              events.data
+                .filter((event) => event.status === 'published')
+                .map((event) => (
+                  <EventThumbnailCard
+                    href={`/admin/event/${event.id}`}
+                    key={event.id}
+                    event={event}
+                  />
+                ))}
+          </TabsPrimitive.Content>
+          <TabsPrimitive.Content
+            value="draft"
+            className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"
+          >
+            <NewEventCard href={'/admin/new/event'} />
+
+            {events.data &&
+              events.data
+                .filter((event) => event.status === 'draft')
+                .map((event) => (
+                  <EventThumbnailCard
+                    href={`/admin/event/${event.id}`}
+                    key={event.id}
+                    event={event}
+                  />
+                ))}
+          </TabsPrimitive.Content>
+          <TabsPrimitive.Content
+            value="archived"
+            className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3"
+          >
+            <NewEventCard href={'/admin/new/event'} />
+
+            {events.data &&
+              events.data
+                .filter((event) => event.status === 'archived')
+                .map((event) => (
+                  <EventThumbnailCard
+                    href={`/admin/event/${event.id}`}
+                    key={event.id}
+                    event={event}
+                  />
+                ))}
+          </TabsPrimitive.Content>
+        </div>
+      </TabsPrimitive.Tabs>
     </PageContainer>
+  );
+}
+
+function EventsTabsTrigger({
+  className,
+  ...props
+}: React.ComponentProps<typeof TabsPrimitive.Trigger>) {
+  return (
+    <TabsPrimitive.Trigger
+      className={cn(
+        'text-foreground border-border data-[state=active]:border-accent data-[state=active]:text-accent data-[state=active]:bg-accent-50 dark:data-[state=active]:bg-accent-950 focus-visible:ring-offset-background focus-visible:ring-ring flex items-center justify-center gap-1.5 rounded-lg border px-2.5 py-1.5 text-xs font-medium focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none',
+        className
+      )}
+      {...props}
+    />
   );
 }
