3fd7418a58516555965562c6df3bd7e7ea439979
lennardlohmann <lennard.lohmann@icloud.com>
2025-02-26 09:52:14 +0100

    feat: deleted lucide and instead imported from heroicons

--- src/components/ui/checkbox/checkbox.tsx
+++ src/components/ui/checkbox/checkbox.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { CheckIcon } from '@heroicons/react/16/solid';
 import * as CheckboxPrimitive from '@radix-ui/react-checkbox';
-import { CheckIcon } from 'lucide-react';
 import * as React from 'react';
 
 import { cn } from '@/utils';
