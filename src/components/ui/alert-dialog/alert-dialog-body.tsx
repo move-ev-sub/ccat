129f6850cfb2107c743e44aba3e8810ee10b9660
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-03-02 20:20:44 +0100

    feat: update alert dialog component

--- src/components/ui/alert-dialog/alert-dialog-body.tsx
+++ src/components/ui/alert-dialog/alert-dialog-body.tsx
@@ -4,13 +4,13 @@ import * as React from 'react';
 
 import { cn } from '@/utils';
 
-export function AlertDialogHeader({
+export function AlertDialogBody({
   className,
   ...props
 }: React.ComponentProps<'div'>) {
   return (
     <div
-      data-slot="alert-dialog-header"
+      data-slot="alert-dialog-body"
       className={cn('flex flex-col gap-2 text-left', className)}
       {...props}
     />
--- src/components/ui/alert-dialog/index.ts
+++ src/components/ui/alert-dialog/index.ts
@@ -1,5 +1,15 @@
+/**
+ * A modal dialog that interrupts the user with important content and expects
+ * a response. The Dialog will only be closed when the user clicks either the
+ * AlertDialogCancel or AlertDialogAction component.
+ *
+ * The `AlertDialog` component is powered by `@radix-ui/react-alert-dialog`.
+ *
+ * @see https://www.radix-ui.com/primitives/docs/components/alert-dialog
+ */
 export { AlertDialog } from './alert-dialog';
 export { AlertDialogAction } from './alert-dialog-action';
+export { AlertDialogBody } from './alert-dialog-body';
 export { AlertDialogCancel } from './alert-dialog-cancel';
 export { AlertDialogContent } from './alert-dialog-content';
 export { AlertDialogDescription } from './alert-dialog-desc';
