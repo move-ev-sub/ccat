ed3347ffa134e416ca07422faff2dc1edd94e92b
Ron Bellemann <58035534+1RoBe@users.noreply.github.com>
2025-03-09 23:06:09 +0100

    feat: removed superflous import statements

--- src/components/ui/popover/popover-trigger.tsx
+++ src/components/ui/popover/popover-trigger.tsx
@@ -3,8 +3,6 @@
 import * as React from 'react';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
 
-import { cn } from '@/utils';
-
 export function PopoverTrigger({
   ...props
 }: React.ComponentProps<typeof PopoverPrimitive.Trigger>) {
