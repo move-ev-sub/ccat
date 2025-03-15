ffcc54075982164b2a7f18f564be435b3f40bb93
Ron Bellemann <58035534+1RoBe@users.noreply.github.com>
2025-03-15 17:37:57 +0100

    fix: removed animations

--- src/components/ui/accordion/accordion-trigger.tsx
+++ src/components/ui/accordion/accordion-trigger.tsx
@@ -23,8 +23,6 @@ export function AccordionTrigger({
         {...props}
       >
         {children}
-        {/* <ChevronDownIcon className="h-6 w-6 text-gray-500" /> */}
-
         <ChevronDownIcon className="text-muted-foreground pointer-events-none size-4 shrink-0 translate-y-0.5 transition-transform duration-200" />
       </AccordionPrimitive.Trigger>
     </AccordionPrimitive.Header>
