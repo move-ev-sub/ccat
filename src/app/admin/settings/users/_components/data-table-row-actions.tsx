7d78b34674b240b8e10220568645c36d2596ac93
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-03-15 12:12:15 +0100

    feat: prepare on delete action for row actions

--- src/app/admin/settings/users/_components/data-table-row-actions.tsx
+++ src/app/admin/settings/users/_components/data-table-row-actions.tsx
@@ -3,6 +3,7 @@
 import { Row } from '@tanstack/react-table';
 
 import { Button } from '@/components/ui/button';
+import { ConfirmDestructive } from '@/components/ui/confirm-destructive';
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -10,33 +11,48 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
+import { FullUserProfile } from '@/server/types/profile';
 import { EllipsisHorizontalIcon } from '@heroicons/react/16/solid';
+import React from 'react';
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
 }
 
-export function DataTableRowActions<TData>(
-  {
-    //   row,
-  }: DataTableRowActionsProps<TData>
-) {
+export function DataTableRowActions<TData extends FullUserProfile>({
+  row,
+}: DataTableRowActionsProps<TData>) {
+  const [_open, _setOpen] = React.useState(false);
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="data-[state=open]:bg-muted flex h-8 w-8 p-0"
-        >
-          <EllipsisHorizontalIcon />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem>Passwort zurücksetzen</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem variant="destructive">Konto löschen</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <ConfirmDestructive
+        open={_open}
+        onOpenChange={_setOpen}
+        title="Bist du sicher, dass du dieses Konto löschen möchtest?"
+        auditText={row.original.email}
+      />
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="data-[state=open]:bg-muted flex h-8 w-8 p-0"
+          >
+            <EllipsisHorizontalIcon />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem>Passwort zurücksetzen</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem
+            variant="destructive"
+            onSelect={() => _setOpen(true)}
+          >
+            Konto löschen
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
