235d58448f41ea4b3acf1c0cf90ff67af6533392
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-03-16 17:07:10 +0100

    feat: setup mdx components

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,15 @@
+import createMdx from '@next/mdx';
 import './src/env.js';
 
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
 };
 
-export default nextConfig;
+const withMDX = createMdx({
+  extension: /\.mdx?$/,
+});
+
+export default withMDX(nextConfig);
