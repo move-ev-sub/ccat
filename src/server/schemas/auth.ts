30a777eb3eccf0385f834d8df281a053df43b284
Ron Bellemann <58035534+1RoBe@users.noreply.github.com>
2025-03-19 23:27:39 +0100

    feat: add firstName and lastName to signUpSchema

--- src/server/schemas/auth.ts
+++ src/server/schemas/auth.ts
@@ -50,6 +50,9 @@ const generatePasswordSchema = (): z.ZodObject<{ password: z.ZodString }> => {
 
 export const signUpSchema = z
   .object({
+    // Should names be limited to ASCII characters?
+    firstName: z.string({ message: 'Vorname darf nur Buchstaben enthalten' }),
+    lastName: z.string({ message: 'Nachname darf nur Buchstaben enthalten' }),
     email: z.string().email('Ungültige E-Mail-Adresse'),
     acceptLegal: z
       .boolean({
