762f55f76138eb5b4482551561bb218a09a5c437
lennardlohmann <lennard.lohmann@icloud.com>
2025-04-20 08:08:15 +0200

    feat: add legal/* to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -6,5 +6,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     { url: `${baseUrl}/auth/login`, lastModified: new Date() },
     { url: `${baseUrl}/auth/register`, lastModified: new Date() },
+    { url: `${baseUrl}/legal/impressum`, lastModified: new Date() },
+    { url: `${baseUrl}/legal/datenschutz`, lastModified: new Date() },
   ];
 }
