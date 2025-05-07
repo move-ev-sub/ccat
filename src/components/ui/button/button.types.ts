900eab4531ed63239b60c2a0326aef32535b3abc
Christoph Langer <41869956+chris23lngr@users.noreply.github.com>
2025-05-07 20:40:54 +0200

    style: adjust styles for button component

--- src/components/ui/button/button.types.ts
+++ src/components/ui/button/button.types.ts
@@ -7,9 +7,9 @@ export const buttonVariants = cva(
     // alignment
     'flex items-center justify-center gap-2.5',
     // focus
-    'focus-indicator' +
-      // disabled
-      'disabled:opacity-50 disabled:pointer-events-none',
+    'focus-indicator',
+    // disabled
+    'disabled:opacity-50 disabled:pointer-events-none',
     // svg
     '[&_svg]:shrink-0',
   ],
