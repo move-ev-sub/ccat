a74513b7899e63bd9a8dba833811407f25911331
lennardlohmann <lennard.lohmann@icloud.com>
2025-05-31 08:28:23 +0200

    feat: set the standard tracesSampleRate to 1 and added an evn variable to control it

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -8,7 +8,7 @@ Sentry.init({
   dsn: 'SENTRY_DSN',
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-  tracesSampleRate: 1,
+  tracesSampleRate: parseFloat(process.env.SENTRY_SAMPLE_RATE ?? '1'),
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
